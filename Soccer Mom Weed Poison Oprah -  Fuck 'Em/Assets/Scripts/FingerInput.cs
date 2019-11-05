52a2da4c81b6fedb066e7a3576a69358f8c744d5
Anthony Posselli <anthony.posselli@gmail.com>
2019-11-05 14:38:32 -0700

    Begin adding swiping to the game.

---
+++ Soccer Mom Weed Poison Oprah -  Fuck 'Em/Assets/Scripts/FingerInput.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FingerInput : MonoBehaviour
+{
+    static private bool useMouse = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    static public bool GetInputPresent()
+    {
+        useMouse = Input.touchCount <= 0;
+        return Input.touchCount > 0 || Input.mousePresent;
+    }
+
+    static public bool GetFingerPressed()
+    {
+        if (useMouse)
+        {
+            return Input.GetMouseButtonDown(0);
+        }
+
+        return Input.GetTouch(0).phase == TouchPhase.Began;
+    }
+
+    static public bool GetFingerDown()
+    {
+        if (useMouse)
+        {
+            return Input.GetMouseButton(0);
+        }
+
+        return Input.touchCount > 0;
+    }
+
+    static public bool GetFingerReleased()
+    {
+        if (useMouse)
+        {
+            return Input.GetMouseButtonUp(0);
+        }
+
+        return Input.GetTouch(0).phase == TouchPhase.Ended;
+    }
+
+    static public Vector2 GetFingerPosition()
+    {
+        if (useMouse)
+        {
+            return Input.mousePosition;
+        }
+
+        return Input.GetTouch(0).position;
+    }
+}
---
+++ Soccer Mom Weed Poison Oprah -  Fuck 'Em/Assets/Scripts/SwipeScript.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwipeScript : MonoBehaviour
+{
+    private Camera camera;
+    private Vector3 touchPos;
+    private Vector3 fingerPos;
+    private Vector3 objectPos;
+    private float minSwipeDistance;
+    private bool needsRelease = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        minSwipeDistance = Screen.width * 30 / 100;
+        camera = Camera.main;
+
+        if (FingerInput.GetInputPresent() && FingerInput.GetFingerDown())
+            needsRelease = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!FingerInput.GetInputPresent() || !FingerInput.GetFingerDown())
+        {
+            needsRelease = false;
+            return;
+        }
+
+        if (needsRelease)
+            return;
+
+        fingerPos = FingerInput.GetFingerPosition();
+
+        if (FingerInput.GetFingerPressed())
+        {
+            touchPos = fingerPos;
+            objectPos = gameObject.transform.position;
+        }
+
+        if (Mathf.Abs(touchPos.x - fingerPos.x) >= minSwipeDistance)
+        {
+            if (touchPos.x - fingerPos.x > 0)
+            {
+                Instantiate(gameObject, Vector3.zero, Quaternion.Euler(0f, 0f, 0f));
+                Destroy(gameObject);
+            }
+            else
+            {
+                Instantiate(gameObject, Vector3.zero, Quaternion.Euler(0f, 0f, 0f));
+                Destroy(gameObject);
+            }
+        }
+
+        gameObject.transform.position = objectPos + new Vector3(camera.ScreenToWorldPoint(fingerPos).x - camera.ScreenToWorldPoint(touchPos).x, 0f, 0f);
+    }
+}
