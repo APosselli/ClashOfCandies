c55661104c422f228acea09538ecfecc0d7c4576
Anthony Posselli <anthony.posselli@gmail.com>
2019-11-06 23:40:10 -0700

    Add scoring and a fail state. Fix centering.
    Add UI to make a functional game. Add a GameState class to keep track of the state (score, lives, failure, etc.).

---
+++ Soccer Mom Weed Poison Oprah -  Fuck 'Em/Assets/Scripts/GameState.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameState : MonoBehaviour
+{
+    public GameObject scoreTextObject;
+    public GameObject livesTextObject;
+    public GameObject candyTextObject; // TODO: Remove when we add art assets
+    public GameObject gameOverTextObject;
+    private UnityEngine.UI.Text scoreText;
+    private UnityEngine.UI.Text livesText;
+    private UnityEngine.UI.Text candyText;
+
+    private static GameState globalInstance;
+    private int score = 0;
+    private int lives = 3;
+    private bool waitingForRelease = false;
+    private bool gameOver = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreText = scoreTextObject.GetComponent<UnityEngine.UI.Text>();
+        livesText = livesTextObject.GetComponent<UnityEngine.UI.Text>();
+        candyText = candyTextObject.GetComponent<UnityEngine.UI.Text>();
+
+        globalInstance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!gameOver)
+            return;
+
+        if (!FingerInput.GetInputPresent())
+            return;
+
+        if (FingerInput.GetFingerPressed())
+            waitingForRelease = true;
+
+        if (waitingForRelease && FingerInput.GetFingerReleased())
+            ResetLevel();
+    }
+
+    public static GameState Instance
+    {
+        get { return globalInstance; }
+    }
+
+    public void AddToScore(int points)
+    {
+        score += points;
+        if (score > 999999999 || score < 0)
+        {
+            score = 999999999;
+        }
+
+        scoreText.text = "Score: " + score.ToString();
+    }
+
+    public void DecrementLives()
+    {
+        lives--;
+        livesText.text = "Non-fatal mistakes remaining: " + lives.ToString();
+        if (lives <= 0)
+            InvokeGameOver();
+    }
+
+    public void SetCurrentCandy(bool isPoison)
+    {
+        if (isPoison)
+            candyText.text = "Candy: Poison";
+        else
+            candyText.text = "Candy: Edible";
+    }
+
+    public void InvokeGameOver()
+    {
+        gameOver = true;
+        gameOverTextObject.SetActive(true);
+    }
+
+    private void ResetLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public bool GameOver
+    {
+        get { return gameOver; }
+    }
+}
--- Soccer Mom Weed Poison Oprah -  Fuck 'Em/Assets/Scripts/SwipeScript.cs
+++ Soccer Mom Weed Poison Oprah -  Fuck 'Em/Assets/Scripts/SwipeScript.cs
@@ -4,32 +4,48 @@ using UnityEngine;
 
 public class SwipeScript : MonoBehaviour
 {
-    private Camera camera;
+    public float centerSpeed = 3f;
+    private Camera cam;
     private Vector3 touchPos;
     private Vector3 fingerPos;
     private Vector3 objectPos;
     private float minSwipeDistance;
     private bool needsRelease = false;
+    public bool isPoison { get; set; }
 
     // Start is called before the first frame update
     void Start()
     {
+        int poisonInt = Random.Range(0, 2);
+
+        // TODO: Remove after merging with Jieyi's candy generation script
+        if (poisonInt == 0)
+        {
+            SetIsPoison(true);
+        }
+        else
+        {
+            SetIsPoison(false);
+        }
+
         minSwipeDistance = Screen.width * 30 / 100;
-        camera = Camera.main;
+        cam = Camera.main;
 
         if (FingerInput.GetInputPresent() && FingerInput.GetFingerDown())
             needsRelease = true;
+
+        objectPos = gameObject.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (GameState.Instance.GameOver)
+            return;
+
         if (!FingerInput.GetInputPresent() || !FingerInput.GetFingerDown())
         {
-            if (FingerInput.GetInputPresent() && !needsRelease && FingerInput.GetFingerReleased())
-            {
-                gameObject.transform.position = objectPos;
-            }
+            gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, objectPos, Time.deltaTime * centerSpeed);
 
             needsRelease = false;
             return;
@@ -43,23 +59,55 @@ public class SwipeScript : MonoBehaviour
         if (FingerInput.GetFingerPressed())
         {
             touchPos = fingerPos;
-            objectPos = gameObject.transform.position;
         }
 
         if (Mathf.Abs(touchPos.x - fingerPos.x) >= minSwipeDistance)
         {
             if (touchPos.x - fingerPos.x > 0)
             {
+                if (isPoison)
+                {
+                    GameState.Instance.AddToScore(1);
+                }
+                else
+                {
+                    GameState.Instance.DecrementLives();
+                }
+
+                if (GameState.Instance.GameOver)
+                {
+                    Destroy(gameObject);
+                    return;
+                }
+
+                // TODO: Remove after merging with Jieyi's candy generation script
                 Instantiate(gameObject, Vector3.zero, Quaternion.Euler(0f, 0f, 0f));
+
                 Destroy(gameObject);
             }
             else
             {
+                if (isPoison)
+                {
+                    Destroy(gameObject);
+                    GameState.Instance.InvokeGameOver();
+                    return;
+                }
+
+                GameState.Instance.AddToScore(1);
+                // TODO: Remove after merging with Jieyi's candy generation script
                 Instantiate(gameObject, Vector3.zero, Quaternion.Euler(0f, 0f, 0f));
+
                 Destroy(gameObject);
             }
         }
 
-        gameObject.transform.position = objectPos + new Vector3(camera.ScreenToWorldPoint(fingerPos).x - camera.ScreenToWorldPoint(touchPos).x, 0f, 0f);
+        gameObject.transform.position = objectPos + new Vector3(cam.ScreenToWorldPoint(fingerPos).x - cam.ScreenToWorldPoint(touchPos).x, 0f, 0f);
+    }
+
+    public void SetIsPoison(bool isPoison)
+    {
+        this.isPoison = isPoison;
+        GameState.Instance.SetCurrentCandy(isPoison);
     }
 }
