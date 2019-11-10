cae5f81d2dde9d28f593354c95f26cf3f5330552
Jieyina <nasjieyi@gmail.com>
2019-11-10 15:59:04 -0700

    basic store functions after completing level

---
+++ Soccer Mom Weed Poison Oprah -  Fuck 'Em/Assets/Scripts/StoreButtons.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StoreButtons : MonoBehaviour
+{
+
+    private int candyCost = 9;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void BuyCandy()
+    {
+        if (StoreScript.money > 0 && StoreScript.premCandyNum < 20)
+        {
+            StoreScript.money -= candyCost;
+            StoreScript.premCandyNum++;
+        }
+    }
+
+    public void ContinueGame()
+    {
+        StoreScript.storeActive = false;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
--- Soccer Mom Weed Poison Oprah -  Fuck 'Em/Assets/Scripts/StoreScript.cs
+++ Soccer Mom Weed Poison Oprah -  Fuck 'Em/Assets/Scripts/StoreScript.cs
@@ -5,21 +5,33 @@ using UnityEngine;
 public class StoreScript : MonoBehaviour
 {
     public static bool storeActive = false;
+    private UnityEngine.UI.Text moneyText;
+    private UnityEngine.UI.Text candyText;
+
+    // TODO: replace with gamestate variable after merging with Anothoy's work
+    public static int money = 999;
+    public static int premCandyNum = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        moneyText = transform.Find("StorePanel").Find("MoneyText").gameObject.GetComponent<UnityEngine.UI.Text>();
+        candyText = transform.Find("StorePanel").Find("PremCandyText").gameObject.GetComponent<UnityEngine.UI.Text>();
     }
 
     // Update is called once per frame
     void Update()
     {
         int currentCandyCnt = GameObject.Find("CurrentCandy").transform.childCount;
-        Debug.Log(currentCandyCnt);
         if (currentCandyCnt == 0 && !storeActive)
         {
             transform.Find("StorePanel").gameObject.SetActive(true);
             storeActive = true;
+        }
+        if (storeActive)
+        {
+            moneyText.text = "Money: " + money.ToString();
+            candyText.text = "Premium Candy: " + premCandyNum.ToString();
         }
     }
 }
